3d7a33357421daaaca7da876d6aada48a4baec04
Saswata Banerjee <azure.saswata@gmail.com>
2023-05-30 09:41:36 +0530

    VideoCard and ChannelCard added

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,10 +1,19 @@
+import { Box, Stack } from '@mui/material'
 import React from 'react'
+import {VideoCard, ChannelCard} from './'
 
 const Videos = ({ videos }) => {
-  console.log(videos)
+  // console.log(videos)
 
   return (
-    <div>Videos</div>
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item,idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channel={item} />}
+        </Box>
+      ))}
+    </Stack>
   )
 }
 
