3bc9f11dcb1f18efa57fbcbfaee42da4ca4a5ecb
Saswata Banerjee <azure.saswata@gmail.com>
2023-05-30 08:31:59 +0530

    Videos added to Feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,19 +1,42 @@
-import { Box, Stack, Typography } from '@mui/material'
-import React from 'react'
-import SideBar from './SideBar'
+import { Box, Stack, Typography } from "@mui/material";
+import React from "react";
+import { SideBar, Videos } from "./";
 
 const Feed = () => {
   return (
-    <Stack sx={{ flexDirection: {sx: 'column', md: 'row'} }}>
-      <Box sx={{ height:{sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px:{sx:0,md:2} }}>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
         <SideBar></SideBar>
 
-        <Typography className='copyright' variant='body2' sx={{ mt:1.5, color:'#fff' }}>
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
           &copy; 2023 SB Media
         </Typography>
       </Box>
-    </Stack>
-  )
-}
 
-export default Feed
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New <span style={{ color: "#f31503" }}>videos</span>
+        </Typography>
+
+        <Videos videos={[]} />
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
